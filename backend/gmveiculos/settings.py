0ee79fed1ecb2e5c6c97af2e18edc434a2fb379a
Brunno Oliveira <rwanb.oliveira@gmail.com>
2023-09-29 04:23:25 -0300

    feat(/backend): configura banco de dados

--- backend/gmveiculos/settings.py
+++ backend/gmveiculos/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'veiculos',
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,15 @@ WSGI_APPLICATION = 'gmveiculos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gmveiculos',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
+
 
 
 # Password validation
--- backend/veiculos/admin.py
+++ backend/veiculos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+# Registre cada classe de modelo individualmente
+admin.site.register(CondicoesVeiculos)
+admin.site.register(Marcas)
+admin.site.register(Modelos)
+admin.site.register(Veiculos)
