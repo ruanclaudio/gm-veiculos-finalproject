3764083e48470095d4b36c45df4fcf3d79998e8d
Brunno Oliveira <rwanb.oliveira@gmail.com>
2023-10-09 05:44:24 -0300

    adicao paginacao ao endpoint /veiculos

--- backend/veiculos/views.py
+++ backend/veiculos/views.py
@@ -4,9 +4,18 @@ from .serializers import *
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.core.paginator import Paginator
 
 @api_view(['GET'])
 def veiculos_list(request):
+    # serializa todos os veiculos
     veiculos = Veiculos.objects.all()
     serializer = VeiculoSerializer(veiculos, many = True)
+
+    # faz a paginacao
+    paginator = Paginator(veiculos, 10)
+    
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     return Response(serializer.data)
