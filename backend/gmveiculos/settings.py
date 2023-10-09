a0f9915df76f8ac092345497327b0d9d745dc4c6
Brunno Oliveira <rwanb.oliveira@gmail.com>
2023-10-09 01:48:19 -0300

    fix:ajusta arquitetura e configurações

--- backend/gmveiculos/settings.py
+++ backend/gmveiculos/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
     'veiculos',
 ]
 
@@ -110,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
