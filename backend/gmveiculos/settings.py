20fd5a27bb974fc3251f8f8c7874930a93eb6d8b
Brunno Oliveira <rwanb.oliveira@gmail.com>
2023-10-20 01:56:57 -0300

    fix

--- backend/gmveiculos/settings.py
+++ backend/gmveiculos/settings.py
@@ -47,13 +47,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # cors
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'gmveiculos.urls'
