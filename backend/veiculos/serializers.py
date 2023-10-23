39fb46e1caf09517ee3d866f4e39178ed892e8cb
Brunno Oliveira <rwanb.oliveira@gmail.com>
2023-10-22 22:52:26 -0300

    fix

--- backend/veiculos/serializers.py
+++ backend/veiculos/serializers.py
@@ -1,24 +1,25 @@
 from .models import *
-from rest_framework import serializers 
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
-class CondicaoVeiculoSerializer(serializers.ModelSerializer):
+class CondicaoVeiculoSerializer(ModelSerializer):
     class Meta:
         model = CondicaoVeiculo
-        fields = ['condicao_usado', 'ano', 'leiloado']
+        fields = ['condicao_usado', 'ano', 'leiloado', 'quilometragem']
 
-class MarcaSerializer(serializers.ModelSerializer):
+class MarcaSerializer(ModelSerializer):
     class Meta:
         model = Marca
         fields = '__all__'
     
-class ModeloSerializer(serializers.ModelSerializer):
+class ModeloSerializer(ModelSerializer):
     marca = serializers.StringRelatedField()
 
     class Meta:
         model = Modelo
         fields = ['nome', 'tipo', 'marca']
 
-class VeiculoSerializer(serializers.ModelSerializer):
+class VeiculoSerializer(ModelSerializer):
     modelo = ModeloSerializer()
     condicao = CondicaoVeiculoSerializer()
 
